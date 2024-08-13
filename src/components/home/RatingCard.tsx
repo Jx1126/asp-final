c51c8f2ccb6d13a7e044d6098e50b2c113ed03be
Jx1126 <147470595+Jx1126@users.noreply.github.com>
2024-08-13 20:43:40 +0800

    Added star ratings into rating cards

--- src/components/home/RatingCard.tsx
+++ src/components/home/RatingCard.tsx
@@ -9,7 +9,22 @@ const RatingCard = ({ star, message, avatar, username }: Props) => (
   <div className="flex justify-center bg-slate-200 border-slate-300 max-w-72 border-2 shadow-inner rounded-lg min-h-full">
 
     <div className="flex flex-col gap-3 my-5">
-      <p>{star}</p>
+
+      <div className="flex items-center justify-center">
+        <div className="rating my-2">
+          {[1, 2, 3, 4, 5].map((value) => (
+            <input
+              key={value}
+              type="radio"
+              name={`rating-${username}`}
+              className="mask mask-star-2 bg-orange-400"
+              checked={value <= star}
+              readOnly
+            />
+          ))}
+        </div>
+      </div>
+
       <p className="max-w-56 font-semibold">" {message} "</p>
 
       <div className="flex flex-row gap-4 items-center mt-3">
