8d72f7d38c4190f3cacf3795e9bb03c9b6afe0a1
gabagaba <68807691+gwwj@users.noreply.github.com>
2024-08-17 23:03:23 +0800

    Basic Calendar Functionality
    - Set up basic structure using react-big-calendar package

--- src/components/finance/Finance.tsx
+++ src/components/finance/Finance.tsx
@@ -1,9 +1,30 @@
+import { Calendar, momentLocalizer, Event } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const localizer = momentLocalizer(moment);
+
+const events: Event[] = [
+  {
+    title: "Meeting",
+    start: new Date(),
+    end: new Date(),
+    allDay: false,
+  },
+];
+
 function Finance() {
   return (
     <div>
-      <h1 className="font-bold text-5xl" >Finance</h1>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
     </div>
-  )
+  );
 }
 
 export default Finance;
