008a597daf172043c7d7707cb40c57b75b0451b6
Jx1126 <147470595+Jx1126@users.noreply.github.com>
2024-08-18 16:34:18 +0800

    Added close toast function in the alert toast

--- src/components/common/Toast.tsx
+++ src/components/common/Toast.tsx
@@ -1,9 +1,12 @@
+import { useState } from 'react';
 interface Props {
   type: 'success' | 'error' | 'warning';
   message: string;
 }
 
 const Toast = ({ type, message }: Props) => {
+
+  const [showToast, setShowToast] = useState(true);
 
   const getIcon = () => {
     switch (type) {
@@ -33,17 +36,23 @@ const Toast = ({ type, message }: Props) => {
       case 'warning':
         return 'bg-yellow-100 border-yellow-400 text-yellow-700';
       default:
-        return null;
+        return '';
     }
   }
+
+  const closeToast = () => {
+    setShowToast(false);
+  }
+
+  if(!showToast) return null;
 
   return (
     <>
       <div role="alert" className={`alert flex skeleton border rounded-md px-4 py-3 max-w-xl fixed w-full z-[1] ${getDesign()}`}>
         {getIcon()}
         <span>{message}</span>
-        <button className="absolute top-0 bottom-0 right-0 px-4 py-3">
-            <svg className="fill-current opacity-65 h-6 w-6" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+        <button onClick={closeToast} className="absolute top-0 bottom-0 right-0 px-4 py-3">
+            <svg className="fill-current opacity-65 hover:opacity-85 ease-in-out transition-all h-6 w-6" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
         </button>
       </div>
     </>
