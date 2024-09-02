1b25463129cecab36f9d8e7faa316de17b2856ab
gabagaba <68807691+gwwj@users.noreply.github.com>
2024-09-03 04:15:28 +0800

    Transaction Database Backend
    - Added SQLite Database
    - Linked database to components requiring transaction data
    - Namely FinanceCalendar, MonthlyFinanceData and YearlySummaryData

---
+++ src/components/finance/FinanceCalendarOriginal.tsx
@@ -0,0 +1,70 @@
+import { Calendar, momentLocalizer, Event } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { startOfYear, endOfYear, eachDayOfInterval } from "date-fns";
+
+const localizer = momentLocalizer(moment);
+
+interface Props {
+  budget: number;
+}
+
+function FinanceCalendar({ budget }: Props) {
+  function getYearArray(date) {
+    let yearStart = startOfYear(todayDate);
+    let yearEnd = endOfYear(todayDate);
+
+    let currentYearArray = eachDayOfInterval({
+      start: yearStart,
+      end: yearEnd,
+    });
+
+    return currentYearArray;
+  }
+
+  function generateEventArray(dateRange) {
+    let eventArray = [];
+    for (let i = 0; i < dateRange.length; i++) {
+      let day = {
+        title: Math.floor(
+          (budget * 12) / dateRange.length - Math.floor(Math.random() * 1000)
+        ),
+        start: dateRange[i],
+        end: dateRange[i],
+        allDay: false,
+      };
+      eventArray.push(day);
+    }
+    return eventArray;
+  }
+
+  function eventColour(event) {
+    let backgroundColor;
+    if (event.title < 0) {
+      backgroundColor = "#f28482";
+    } else {
+      backgroundColor = "#90a955";
+    }
+    return { style: { backgroundColor } };
+  }
+
+  let todayDate = new Date();
+  let yearArray = getYearArray(todayDate);
+  let events = generateEventArray(yearArray);
+
+  return (
+    <>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: "70vh", minHeight: "400px" }}
+        views={["month"]}
+        eventPropGetter={eventColour}
+      />
+    </>
+  );
+}
+
+export default FinanceCalendar;
--- src/components/finance/YearlySummaryData.tsx
+++ src/components/finance/YearlySummaryData.tsx
@@ -1,236 +1,137 @@
-import React, { useState } from 'react';
-import YearlySummary from './YearlySummary';
+import React, { useState, useEffect } from "react";
+import YearlySummary from "./YearlySummary"; // Make sure you have this component defined
 
 interface YearlyData {
-    year: number;
-    income: { month: string; amount: number }[];
-    expenses: { month: string; amount: number }[];
-    summary: {
-        yearlyIncome: number;
-        expenses: number;
-        recurringSavings: number;
-        availableDisposableIncome: number;
-    };
+  year: number;
+  income: { month: string; amount: number }[];
+  expenses: { month: string; amount: number }[];
+  summary: {
+    yearlyIncome: number;
+    expenses: number;
+    recurringSavings: number;
+    availableDisposableIncome: number;
+  };
 }
 
-const yearlyData: YearlyData[] = [
-    {
-        year: 2024,
-        income: [
-            { month: 'January', amount: 7184.64 },
-            { month: 'February', amount: 9427.29 },
-            { month: 'March', amount: 7522.21 },
-            { month: 'April', amount: 8858.9 },
-            { month: 'May', amount: 8600.2 },
-            { month: 'June', amount: 7224.5 },
-            { month: 'July', amount: 9238.1 },
-            { month: 'August', amount: 8778.2 },
-        ],
-        expenses: [
-            { month: 'January', amount: 1590.31 },
-            { month: 'February', amount: 2031.9 },
-            { month: 'March', amount: 1105.21 },
-            { month: 'April', amount: 3411.2 },
-            { month: 'May', amount: 1300.2 },
-            { month: 'June', amount: 2021.1 },
-            { month: 'July', amount: 1101.1 },
-            { month: 'August', amount: 1563.2 },
-        ],
-        summary: {
-            yearlyIncome: 66834.04,
-            expenses: 14124.22,
-            recurringSavings: 12000.0,
-            availableDisposableIncome: 40709.82,
-        },
+async function getYearMonthType(year, month, type) {
+  let urlString = `${URL}/yearmonthtype?year=${year}&month=${month}&type=${type}`;
+  let data = await fetch(urlString);
+  let response = await data.json();
+  return response["SUM(amount)"];
+}
 
-    },
-    {
-        year: 2023,
-        income: [
-            { month: 'January', amount: 7184.64 },
-            { month: 'February', amount: 9427.29 },
-            { month: 'March', amount: 7522.21 },
-            { month: 'April', amount: 8858.9 },
-            { month: 'May', amount: 8600.2 },
-            { month: 'June', amount: 7224.5 },
-            { month: 'July', amount: 9238.1 },
-            { month: 'August', amount: 8778.2 },
-            { month: 'September', amount: 8520.2 },
-            { month: 'October', amount: 7094.5 },
-            { month: 'November', amount: 9228.1 },
-            { month: 'December', amount: 8058.2 },
-        ],
-        expenses: [
-            { month: 'January', amount: 1590.31 },
-            { month: 'February', amount: 2031.9 },
-            { month: 'March', amount: 1105.21 },
-            { month: 'April', amount: 3411.2 },
-            { month: 'May', amount: 1300.2 },
-            { month: 'June', amount: 2021.1 },
-            { month: 'July', amount: 1101.1 },
-            { month: 'August', amount: 4123.2 },
-            { month: 'September', amount: 2312.2 },
-            { month: 'October', amount: 1123.5 },
-            { month: 'November', amount: 2313.1 },
-            { month: 'December', amount: 2012.2 },
-        ],
-        summary: {
-            yearlyIncome: 99735.04,
-            expenses: 24445.22,
-            recurringSavings: 12000.0,
-            availableDisposableIncome: 63289.82,
-        },
+async function getYearType(year, type) {
+  let urlString = `${URL}/yeartype?year=${year}&type=${type}`;
+  let data = await fetch(urlString);
+  let response = await data.json();
+  return response["SUM(amount)"];
+}
 
-    },
-    {
-        year: 2022,
-        income: [
-            { month: 'January', amount: 7184.64 },
-            { month: 'February', amount: 9427.29 },
-            { month: 'March', amount: 7522.21 },
-            { month: 'April', amount: 8858.9 },
-            { month: 'May', amount: 8600.2 },
-            { month: 'June', amount: 7224.5 },
-            { month: 'July', amount: 9238.1 },
-            { month: 'August', amount: 8778.2 },
-            { month: 'September', amount: 8520.2 },
-            { month: 'October', amount: 7094.5 },
-            { month: 'November', amount: 9228.1 },
-            { month: 'December', amount: 8058.2 },
-        ],
-        expenses: [
-            { month: 'January', amount: 1590.31 },
-            { month: 'February', amount: 2031.9 },
-            { month: 'March', amount: 1105.21 },
-            { month: 'April', amount: 3411.2 },
-            { month: 'May', amount: 1300.2 },
-            { month: 'June', amount: 2021.1 },
-            { month: 'July', amount: 1101.1 },
-            { month: 'August', amount: 4123.2 },
-            { month: 'September', amount: 2312.2 },
-            { month: 'October', amount: 1123.5 },
-            { month: 'November', amount: 2313.1 },
-            { month: 'December', amount: 2012.2 },
-        ],
-        summary: {
-            yearlyIncome: 99735.04,
-            expenses: 24445.22,
-            recurringSavings: 12000.0,
-            availableDisposableIncome: 63289.82,
-        },
+const URL = "http://localhost:3001"; // Replace with your actual API URL
 
-    },
-    {
-        year: 2021,
-        income: [
-            { month: 'January', amount: 7184.64 },
-            { month: 'February', amount: 9427.29 },
-            { month: 'March', amount: 7522.21 },
-            { month: 'April', amount: 8858.9 },
-            { month: 'May', amount: 8600.2 },
-            { month: 'June', amount: 7224.5 },
-            { month: 'July', amount: 9238.1 },
-            { month: 'August', amount: 8778.2 },
-            { month: 'September', amount: 8520.2 },
-            { month: 'October', amount: 7094.5 },
-            { month: 'November', amount: 9228.1 },
-            { month: 'December', amount: 8058.2 },
-        ],
-        expenses: [
-            { month: 'January', amount: 1590.31 },
-            { month: 'February', amount: 2031.9 },
-            { month: 'March', amount: 1105.21 },
-            { month: 'April', amount: 3411.2 },
-            { month: 'May', amount: 1300.2 },
-            { month: 'June', amount: 2021.1 },
-            { month: 'July', amount: 1101.1 },
-            { month: 'August', amount: 4123.2 },
-            { month: 'September', amount: 2312.2 },
-            { month: 'October', amount: 1123.5 },
-            { month: 'November', amount: 2313.1 },
-            { month: 'December', amount: 2012.2 },
-        ],
-        summary: {
-            yearlyIncome: 99735.04,
-            expenses: 24445.22,
-            recurringSavings: 12000.0,
-            availableDisposableIncome: 63289.82,
-        },
-
-    },
-    {
-        year: 2020,
-        income: [
-            { month: 'January', amount: 7184.64 },
-            { month: 'February', amount: 9427.29 },
-            { month: 'March', amount: 7522.21 },
-            { month: 'April', amount: 8858.9 },
-            { month: 'May', amount: 8600.2 },
-            { month: 'June', amount: 7224.5 },
-            { month: 'July', amount: 9238.1 },
-            { month: 'August', amount: 8778.2 },
-            { month: 'September', amount: 8520.2 },
-            { month: 'October', amount: 7094.5 },
-            { month: 'November', amount: 9228.1 },
-            { month: 'December', amount: 8058.2 },
-        ],
-        expenses: [
-            { month: 'January', amount: 1590.31 },
-            { month: 'February', amount: 2031.9 },
-            { month: 'March', amount: 1105.21 },
-            { month: 'April', amount: 3411.2 },
-            { month: 'May', amount: 1300.2 },
-            { month: 'June', amount: 2021.1 },
-            { month: 'July', amount: 1101.1 },
-            { month: 'August', amount: 4123.2 },
-            { month: 'September', amount: 2312.2 },
-            { month: 'October', amount: 1123.5 },
-            { month: 'November', amount: 2313.1 },
-            { month: 'December', amount: 2012.2 },
-        ],
-        summary: {
-            yearlyIncome: 99735.04,
-            expenses: 24445.22,
-            recurringSavings: 12000.0,
-            availableDisposableIncome: 63289.82,
-        },
-
-    },
-    // Add more years here...
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
 ];
 
+let yearsArray = [2020, 2021, 2022, 2023, 2024];
+
 const YearlySummaryData: React.FC = () => {
-    const [activeYear, setActiveYear] = useState<number | null>(null);
+  const [activeYear, setActiveYear] = useState<number | null>(null);
+  const [yearlyData, setYearlyData] = useState<YearlyData[]>([]);
 
-    const toggleYear = (year: number) => {
-        setActiveYear(activeYear === year ? null : year);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let yearlyDataArray = [];
+
+        for (let year of yearsArray) {
+          let income = [];
+          let expenses = [];
+          for (let i = 0; i < 12; i++) {
+            let monthName = monthNames[i];
+            let incomeAmount = await getYearMonthType(year, i, "Income"); // 0-indexed months
+            income.push({
+              month: monthName,
+              amount: incomeAmount || 0,
+            });
+            let expensesAmount = await getYearMonthType(year, i, "Expense"); // 0-indexed months
+            expenses.push({
+              month: monthName,
+              amount: expensesAmount || 0,
+            });
+          }
+
+          let yearlyIncome = await getYearType(year, "Income");
+          let yearlyExpenses = await getYearType(year, "Expense");
+          let recurringSavings = 12000;
+          let availableDisposableIncome =
+            (yearlyIncome || 0) - (yearlyExpenses || 0) - recurringSavings;
+
+          yearlyDataArray.push({
+            year: year,
+            income: income,
+            expenses: expenses,
+            summary: {
+              yearlyIncome: yearlyIncome || 0,
+              expenses: yearlyExpenses || 0,
+              recurringSavings: recurringSavings,
+              availableDisposableIncome: availableDisposableIncome,
+            },
+          });
+        }
+
+        setYearlyData(yearlyDataArray as YearlyData[]);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        // Handle the error appropriately (e.g., display an error message to the user)
+      }
     };
-    const activeYearData = yearlyData.find((data) => data.year === activeYear);
 
-    return (
-        <div className="yearly-summary-data">
-            <div className="year-list">
-                {yearlyData.map((data) => (
-                    <div className='border-gray-300 rounded-lg mt-4 mb-4 border-2 shadow-inner'>
-                        <div key={data.year} className='text-center'>
-                            <div
-                                className="year-button bg-gray-100 p-4 cursor-pointer font-semibold hover:bg-cyan-100 transition"
-                                onClick={() => toggleYear(data.year)}
-                            >
-                                {data.year}
-                            </div>
-                            {activeYear === data.year && activeYearData && (
-                                <div className="bg-white p-4">
-                                    {/* Display detailed financial information */}
-                                    <YearlySummary data={activeYearData} />
-                                </div>
-                            )}
-                        </div>
-                    </div>
-                ))}
+    fetchData();
+  }, []);
+
+  const toggleYear = (year: number) => {
+    setActiveYear(activeYear === year ? null : year);
+  };
+  const activeYearData = yearlyData.find((data) => data.year === activeYear);
+
+  return (
+    <div className="yearly-summary-data">
+      <div className="year-list">
+        {yearlyData.map((data) => (
+          <div
+            className="border-gray-300 rounded-lg mt-4 mb-4 border-2 shadow-inner"
+            key={data.year}
+          >
+            <div className="text-center">
+              <div
+                className="year-button bg-gray-100 p-4 cursor-pointer font-semibold hover:bg-cyan-100 transition"
+                onClick={() => toggleYear(data.year)}
+              >
+                {data.year}
+              </div>
+              {activeYear === data.year && activeYearData && (
+                <div className="bg-white p-4">
+                  <YearlySummary data={activeYearData} />
+                </div>
+              )}
             </div>
-        </div>
-    )
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-}
 export default YearlySummaryData;
